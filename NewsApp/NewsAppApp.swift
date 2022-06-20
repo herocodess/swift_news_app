eaccdf19d10ac1ad7e07c09b9b652f10e8745132
Momoh Hero <momohhero2@gmail.com>
2022-06-20 19:00:13 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Hero Momoh on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
