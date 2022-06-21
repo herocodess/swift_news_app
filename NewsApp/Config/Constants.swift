7c2ab6b350633095cc2114715d9960bf67fbd2db
Momoh Hero <momohhero2@gmail.com>
2022-06-21 23:14:00 +0100

    Built Settings screen and added Constants file

---
+++ NewsApp/Config/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NewsApp
+//
+//  Created by Hero Momoh on 21/06/2022.
+//
+
+import Foundation
+
+enum Constants {
+    static let twitter = "https://twitter.com/herocodes_"
+    static let email = "mailto:herocodess@gmail.com"
+    static let phone = "tel:0909090909"
+}
--- NewsApp/Views/SettingsView.swift
+++ NewsApp/Views/SettingsView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section (header: Text("Display"),
+                         footer: Text("System setting will override Dark mode and use the current device theme")
+                            ) {
+                    Toggle(isOn: .constant(true), label: {
+                        Text("Dark mode")
+                    })
+                    Toggle(isOn: .constant(true), label: {
+                        Text("Use system settings")
+                    })
+                }
+                
+                Section {
+                    Link(destination: URL(string: Constants.twitter)!, label: {
+                        Label("Follow me on twitter @herocodes_", systemImage: "link")
+                    })
+                    
+                    Link("Contact me via email",destination: URL(string: Constants.email)!)
+                    
+                    Link("Call me",destination: URL(string: Constants.phone)!)
+                        
+                        
+                }
+                .foregroundColor(.black)
+                .font(.system(size: 14, weight: .medium))
+
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
